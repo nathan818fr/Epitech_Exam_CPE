8e5115c528f2d6dcf46d25a28ef30511f92ad6f4
Nathan Poirier <nathan@poirier.io>
2015-01-22 14:23:12 +0100

    add aff_oklm_by_lucas

---
+++ 0_aff_oklm/aff_oklm_by_lucas.c
@@ -0,0 +1,26 @@
+/*
+** aff_oklm_by_lucas.c for aff_oklm in /home/poirie-n/blinux/poirie_n/repo/MyProjects/Epitech_Exam_CPE/0_aff_oklm
+** 
+** Made by Nathan Poirier
+** Login   <poirie-n@epitech.net>
+** 
+** Started on  Thu Jan 22 14:20:57 2015 Nathan Poirier
+** Last update Thu Jan 22 14:22:39 2015 Nathan Poirier
+*/
+
+/*
+** AFF_OKLM BY LUCAS !
+**
+** Essaye de trouver toutes les erreurs !
+*/
+
+int	my_putchar(char *c)
+{
+  write(1, c, 1);
+}
+
+int	main()
+{
+  my_putchar('oklm\n');
+}
+  
