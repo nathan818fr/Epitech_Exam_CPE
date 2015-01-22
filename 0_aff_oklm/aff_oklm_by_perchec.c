5c7b6f9ce282d46dba8f7c3b7501aa383bf31803
Nathan Poirier <nathan@poirier.io>
2015-01-22 14:20:29 +0100

    add header to aff_oklm_by_perchec

--- 0_aff_oklm/aff_oklm_by_perchec.c
+++ 0_aff_oklm/aff_oklm_by_perchec.c
@@ -1,3 +1,13 @@
+/*
+** aff_oklm_by_perchec.c for aff_oklm in /home/poirie-n/blinux/poirie_n/repo/MyProjects/Epitech_Exam_CPE/0_aff_oklm
+** 
+** Made by Kévin Empociello
+** Login   <poirie-n@epitech.net>
+** 
+** Started on  Sun Dec 28 18:00:49 2015 Kévin Empociello
+** Last update Thu Jan 22 14:20:11 2015 Nathan Poirier
+*/
+
 void  my_putchar(char c)
 {
   write(1, &c, 1);
